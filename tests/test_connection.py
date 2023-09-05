c2d95b961fa5783f9fb968c738d85b836cf24c94
IlliaLobanok <i.l.lobanok@gmail.com>
2023-09-05 14:57:30 +0300

    + test for connection with DB altered test_connection_manager (rename)

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,7 +1,20 @@
+import mysql.connector
 from connection_manager import ConnectionManager
 
 
 def test_connection():
+    test_conn = mysql.connector.pooling.MySQLConnectionPool(
+        pool_name="ConnectionManagerPool",
+        host="127.0.0.1",
+        user="root",
+        password="",
+        database="PIS_schema")
+    connection = test_conn.get_connection()
+    assert connection.is_connected() is True
+    connection.close()
+
+
+def test_connection_manager():
     connection_manager = ConnectionManager()
     connection = connection_manager.get_connection()
     assert connection.is_connected() is True
